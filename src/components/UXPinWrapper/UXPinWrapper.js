c9f3ca82ac7e178c974b843aa6c25bb18d8f2c0c
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2023-01-25 11:04:27 -0700

    Added Font Awesome

--- src/components/UXPinWrapper/UXPinWrapper.js
+++ src/components/UXPinWrapper/UXPinWrapper.js
@@ -5,9 +5,15 @@ import {
   defineCustomElements,
 } from "@department-of-veterans-affairs/component-library/";
 
+const fontAwesome = document.createElement('link');
+fontAwesome.setAttribute('href', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css');
+fontAwesome.setAttribute('rel', 'stylesheet');
+document.head.appendChild(fontAwesome);
+
 export default function UXPinWrapper({ children }) {
   applyPolyfills().then(() => {
     defineCustomElements();
-  });  
+  });
+  
   return children;
 }
