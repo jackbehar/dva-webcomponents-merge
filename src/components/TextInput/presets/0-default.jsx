8c7dd4d9a384fc95b41f05f0509e1f3d726a6801
Andrew Steele <andrew.steele@adhocteam.us>
2023-05-02 11:39:43 -0400

    Adding memorable date

--- src/components/TextInput/presets/0-default.jsx
+++ src/components/TextInput/presets/0-default.jsx
@@ -1,4 +1,4 @@
 import * as React from "react";
 import TextInput from "../TextInput";
 
-export default <TextInput uxpId="textInput-1" />;
+export default <TextInput uxpId="textInput-1" label="Text Input Label" />;
--- uxpin.config.js
+++ uxpin.config.js
@@ -4,12 +4,13 @@ module.exports = {
       {
         name: "General",
         include: [
-          "src/components/Button/Button.js",
           "src/components/Accordion/Accordion.js",
           "src/components/AccordionItem/AccordionItem.js",
-          "src/components/TextInput/TextInput.js",
+          "src/components/Button/Button.js",
           "src/components/Checkbox/Checkbox.js",
           "src/components/CheckboxGroup/CheckboxGroup.js",
+          "src/components/MemorableDate/MemorableDate.js",
+          "src/components/TextInput/TextInput.js",
         ],
       },
     ],
