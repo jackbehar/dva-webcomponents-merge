10af4cd6ad14f45e0096096bba087c0ca7557e94
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2023-05-26 12:29:53 -0600

    clean-up

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,9 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
-import "./styles.scss"
 
 function Button(props) {
-  return <><va-button {...props} /></>
+  return <va-button {...props} />
 }
 
 Button.propTypes = {
